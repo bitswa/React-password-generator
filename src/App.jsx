5e148ae9fc9f0ec0058b89f2d16561babe6c4b4a
Gabriel Moura do Val <bitswa@pm.me>
2022-09-04 16:39:30 -0300

    starting new design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Clipboard from "./icons/Clipboard";
 
 function App() {
   const [lower, setLower] = useState(false);
   const [upper, setUpper] = useState(false);
   const [number, setNumber] = useState(false);
+  const [symbol, setSymbol] = useState(false);
 
   const [selectedList, setSelectedList] = useState([]);
   const [password, setPassword] = useState("");
@@ -70,13 +72,15 @@ function App() {
 
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
+  const symbols = ["!", "@", "#", "$", "%", "&", "*", ".", "+", "-", "="];
+
   const handleSubmit = () => {
-    if (!lower && !upper && !number) return;
+    if (!lower && !upper && !number && !symbol) return;
 
     setPassword("");
     for (let i = 0; i < length; i++) {
       const randomNumber = parseInt(Math.random() * selectedList.length);
-      setPassword((prev) => [...prev, selectedList[randomNumber]]);
+      setPassword((prev) => prev + selectedList[randomNumber]);
     }
   };
 
@@ -93,7 +97,11 @@ function App() {
     if (number) {
       setSelectedList((prev) => [...prev, ...numbers]);
     }
-  }, [lower, upper, number]);
+
+    if (symbol) {
+      setSelectedList((prev) => [...prev, ...symbols]);
+    }
+  }, [lower, upper, number, symbol]);
 
   useEffect(() => {
     console.log(selectedList);
@@ -101,7 +109,16 @@ function App() {
 
   return (
     <div className="App">
-      <div className="password">{password}</div>
+      {/* <div className="password">
+        <h2>{password}</h2>
+        <button
+          title={"copy to clipboard: " + password}
+          className="clipboard"
+          onClick={() => navigator.clipboard.writeText(password)}
+        >
+          <Clipboard />
+        </button>
+      </div> */}
       <form
         className="form"
         onSubmit={(e) => {
@@ -110,50 +127,65 @@ function App() {
         }}
       >
         length: {length}
-        <label className="form__length_label">
-          <span>4</span>
-          <input
-            type="range"
-            min={4}
-            max={32}
-            value={length}
-            onChange={(e) => setLength(e.target.value)}
-          />
-          <span>32</span>
-        </label>
-        <label className="switch">
-          Lowercase
-          <input
-            type="checkbox"
-            className="hidden-toggle"
-            onChange={(e) => setLower(e.target.checked)}
-          />
-          <div className="slider">
-            <button></button>
-          </div>
-        </label>
-        <label className="switch">
-          Uppercase
-          <input
-            type="checkbox"
-            className="hidden-toggle"
-            onChange={(e) => setUpper(e.target.checked)}
-          />
-          <div className="slider">
-            <button></button>
-          </div>
-        </label>
-        <label className="switch">
-          Numbers
-          <input
-            type="checkbox"
-            className="hidden-toggle"
-            onChange={(e) => setNumber(e.target.checked)}
-          />
-          <div className="slider">
-            <button></button>
-          </div>
-        </label>
+       
+          <label className="form__length_label">
+            <span>4</span>
+            <input
+              type="range"
+              min={4}
+              max={32}
+              value={length}
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <span>32</span>
+          </label>
+        
+        <div className="form__options">
+          <label className="switch">
+            Lowercase
+            <input
+              type="checkbox"
+              className="hidden-toggle"
+              onChange={(e) => setLower(e.target.checked)}
+            />
+            <div className="slider">
+              <button></button>
+            </div>
+          </label>
+          <label className="switch">
+            Uppercase
+            <input
+              type="checkbox"
+              className="hidden-toggle"
+              onChange={(e) => setUpper(e.target.checked)}
+            />
+            <div className="slider">
+              <button></button>
+            </div>
+          </label>
+          <label className="switch">
+            Numbers
+            <input
+              type="checkbox"
+              className="hidden-toggle"
+              onChange={(e) => setNumber(e.target.checked)}
+            />
+            <div className="slider">
+              <button></button>
+            </div>
+          </label>
+          <label className="switch">
+            Symbols
+            <input
+              type="checkbox"
+              className="hidden-toggle"
+              onChange={(e) => setSymbol(e.target.checked)}
+            />
+            <div className="slider">
+              <button></button>
+            </div>
+          </label>
+        </div>
         <button className="submit">Submit</button>
       </form>
     </div>
